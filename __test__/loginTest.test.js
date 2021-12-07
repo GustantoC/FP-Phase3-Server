304be1c616dc93463457d65de702b2b6cb645c30
santomargenmanurung <santomargen@outlook.com>
2021-12-07 16:04:02 +0700

    user register and login test

---
+++ __test__/loginTest.test.js
@@ -0,0 +1,110 @@
+const request = require("supertest");
+const app = require("../app");
+const { User } = require("../models");
+
+beforeAll((done) => {
+  const adminLoginTest = {
+    name: "test",
+    passportNumber: "09437410364326",
+    role: "Admin",
+    email: "test1@mail.com",
+    password: "password",
+    phoneNumber: "111333",
+    status: "Active",
+  };
+  User.create(adminLoginTest)
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+afterAll((done) => {
+  User.destroy({
+    truncate: true,
+    cascade: true,
+    restartIdentity: true,
+  })
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      done(err);
+    });
+});
+
+const loginSuccess = {
+  email: "test1@mail.com",
+  password: "password",
+};
+describe("POST /login user/admin [SUCCESS CASE]", () => {
+  test("should return status 200, & access_token", (done) => {
+    request(app)
+      .post("/login")
+      .send(loginSuccess)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { status, body } = res;
+        expect(status).toBe(200);
+        expect(body).toHaveProperty("access_token", expect.any(String));
+        return done();
+      });
+  });
+});
+
+let loginError = {
+  email: "test1@mail.com",
+  password: "password",
+};
+describe("POST /login user/admin [FAILED CASE]", () => {
+  test("should return status 400 & message when email null", (done) => {
+    let emailNull = {
+      ...loginError,
+      email: null,
+    };
+    request(app)
+      .post("/login")
+      .send(emailNull)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { status, body } = res;
+        expect(status).toBe(400);
+        expect(body).toHaveProperty("message", expect.any(String));
+        return done();
+      });
+  });
+  test("should return status 400 & message when password null", (done) => {
+    let passwordNull = {
+      ...loginError,
+      password: null,
+    };
+    request(app)
+      .post("/login")
+      .send(passwordNull)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { status, body } = res;
+        expect(status).toBe(400);
+        expect(body).toHaveProperty("message", expect.any(String));
+        return done();
+      });
+  });
+  test("should return status 400 & message when authorized", (done) => {
+    let passwordNull = {
+      ...loginError,
+      email: "user@MediaList.com",
+    };
+    request(app)
+      .post("/login")
+      .send(passwordNull)
+      .end((err, res) => {
+        if (err) return done(err);
+        const { status, body } = res;
+        expect(status).toBe(401);
+        expect(body).toHaveProperty("message", expect.any(String));
+        return done();
+      });
+  });
+});
--- router/index.js
+++ router/index.js
@@ -1,24 +1,24 @@
-const router = require('express').Router();
-const UserRouter = require('./UserRouter')
-const StaffRouter = require('./StaffRouter')
-const HistoryRouter = require('./HistoryRouter')
-const LocationRouter = require('./LocationRouter')
-const QuarantineRouter = require('./QuarantineRouter')
-const Authentication = require('../middlewares/Authentication')
-const ErrorHandler = require('../middlewares/ErrorHandler')
-const UserController = require('../controllers/UserController')
+const router = require("express").Router();
+const UserRouter = require("./UserRouter");
+const StaffRouter = require("./StaffRouter");
+const HistoryRouter = require("./HistoryRouter");
+const LocationRouter = require("./LocationRouter");
+const QuarantineRouter = require("./QuarantineRouter");
+const Authentication = require("../middlewares/Authentication");
+const ErrorHandler = require("../middlewares/ErrorHandler");
+const UserController = require("../controllers/UserController");
 
-router.get('/', (_, res) => {
-  res.json({ message: 'Welcome to the API' })
-})
-router.post('/regisAdmin',UserController.regisAdmin)
-router.post('/login', UserController.Login)
-router.use(Authentication)
-router.use('/users', UserRouter)
-router.use('/staffs', StaffRouter)
-router.use('/histories', HistoryRouter)
-router.use('/locations', LocationRouter)
-router.use('/quarantines', QuarantineRouter)
-router.use(ErrorHandler)
+router.get("/", (_, res) => {
+  res.json({ message: "Welcome to the API" });
+});
+router.post("/regisAdmin", UserController.regisAdmin);
+router.post("/login", UserController.Login);
+// router.use(Authentication)
+router.use("/users", UserRouter);
+router.use("/staffs", StaffRouter);
+router.use("/histories", HistoryRouter);
+router.use("/locations", LocationRouter);
+router.use("/quarantines", QuarantineRouter);
+router.use(ErrorHandler);
 
-module.exports = router
+module.exports = router;
