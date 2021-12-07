b70b2be31d19fe1d7ddb219c8a2d4421f6f9c12e
GustantoC <GustantoC@gmail.com>
2021-12-07 14:59:00 +0700

    Create Read Location

--- controllers/LocationController.js
+++ controllers/LocationController.js
@@ -1,20 +1,46 @@
-const { QuarantineLocation } = require('../models')
+const { QuarantineLocation, QuarantineDetail } = require('../models')
 
 
 class LocationController {
   //POST QuarantineLocation = Yang diisi oleh admin doang
   static async createNewLocation(req, res, next) {
+    try {
+      let { name, address, type } = req.body
+      if(type !== 'Wisma' && type !== 'Hotel'){
+        throw { name: '400', message: 'Type must be Wisma or Hotel' }
+      }
+      let newLocation = await QuarantineLocation.create({
+        name,
+        address,
+        type
+      })
+      res.status(201).json({
+        id: newLocation.id,
+        name: newLocation.name,
+        address: newLocation.address,
+        type: newLocation.type
+      })
+    } catch (error) {
+      next(error)
+    }
   }
 
 
   //GET User location detail untuk user
   static async getUserLocation(req, res, next) {
-    
+
   }
 
   //GET All location
   static async getAllLocations(req, res, next) {
-
+    try {
+      const locations = await QuarantineLocation.findAll({
+        attributes: ['id', 'name', 'address', 'type']
+      })
+      res.status(200).json(locations)
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
