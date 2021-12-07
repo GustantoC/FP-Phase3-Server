4bbf1ea595474120c1b300883ed8bfeafe3f116e
santomargenmanurung <santomargen@outlook.com>
2021-12-07 13:40:28 +0700

    unit test register admin done

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 if (process.env.NODE_ENV !== "production") {
-  require('dotenv').config();
+  require("dotenv").config();
 }
 
-const express = require('express')
-const cors = require('cors')
+const express = require("express");
+const cors = require("cors");
 
-const app = express()
-const port = process.env.PORT || 3000
-const router = require('./router');
+const app = express();
+const port = process.env.PORT || 3000;
+const router = require("./router");
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use("/", router);
 
-app.use('/', router)
+// app.listen(port, () => {
+//   console.log("Example app listening at http://localhost:" + port);
+// });
 
-app.listen(port, () => {
-  console.log('Example app listening at http://localhost:' + port)
-})
+module.exports = app;
