39c2a80bdacefeddf4f11b75c86ef22c9669f62f
santomargenmanurung <santomargen@outlook.com>
2021-12-10 15:50:34 +0700

    test change status

--- router/index.js
+++ router/index.js
@@ -9,35 +9,18 @@ const Authentication = require("../middlewares/Authentication");
 const ErrorHandler = require("../middlewares/ErrorHandler");
 const UserController = require("../controllers/UserController");
 
-<<<<<<< HEAD
 router.get("/", (_, res) => {
   res.json({ message: "Welcome to the API" });
 });
-router.post("/regisAdmin", UserController.regisAdmin);
 router.post("/register", UserController.createUser);
 router.post("/login", UserController.Login);
 router.use(Authentication);
 router.use("/users", UserRouter);
+router.use("/trips", TripRouter);
 router.use("/staffs", StaffRouter);
 router.use("/histories", HistoryRouter);
 router.use("/locations", LocationRouter);
 router.use("/quarantines", QuarantineRouter);
 router.use(ErrorHandler);
-=======
-
-router.get('/', (_, res) => {
-  res.json({ message: 'Welcome to the API' })
-})
-router.post('/register',UserController.createUser)
-router.post('/login', UserController.Login)
-router.use(Authentication)
-router.use('/users', UserRouter)
-router.use('/trips', TripRouter)
-router.use('/staffs', StaffRouter)
-router.use('/histories', HistoryRouter)
-router.use('/locations', LocationRouter)
-router.use('/quarantines', QuarantineRouter)
-router.use(ErrorHandler)
->>>>>>> 97a5a73520f4afc7161bc4b9fb594ed304a3626b
 
 module.exports = router;
